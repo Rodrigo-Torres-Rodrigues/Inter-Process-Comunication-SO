3e736d56af06ea608739a5ef332d3a075813a2e1
Rodrigo-Torres-Rodrigues <rodrigotorrest8@gmail.com>
2025-11-22 23:02:09 -0400

    :sparkles:  Added new function

--- funcoes.h
+++ funcoes.h
@@ -63,3 +63,60 @@ int pipeIPC() {
 
     return 0;
 }
+
+
+
+// Semáforo global
+sem_t semaforo;
+
+// Função que cada thread irá executar
+void* processo(void* arg) {
+    int id = *((int*)arg);  // identifica qual "processo" é
+
+    while (1) {
+        printf("Processo %d: tentando acessar o recurso...\n", id);
+
+        // sem_wait = operação DOWN
+        // Se o semáforo está 1 → passa e vira 0
+        // Se está 0 → a thread fica BLOQUEADA pelo sistema
+        sem_wait(&semaforo);
+
+        printf("Processo %d: entrou na seção crítica (utilizando recursos)\n", id);
+
+        // Simula uso do recurso
+        sleep(2);
+
+        printf("Processo %d: saindo da seção crítica\n\n", id);
+
+        // sem_post = operação UP
+        // Libera o recurso → contador volta a 1
+        sem_post(&semaforo);
+
+        // Apenas uma pausa antes de tentar novamente
+        sleep(4);
+    }
+
+    return NULL;
+}
+
+int semaforoSimulacao() {
+    pthread_t t1, t2;   // duas "threads" = dois "processos"
+    int id1 = 1, id2 = 2;
+
+    // Inicializa o semáforo com valor 1 (semáforo binário)
+    sem_init(&semaforo, 0, 1);
+
+    // Cria duas threads que competirão pelo mesmo recurso
+    //O processo é a função void
+    pthread_create(&t1, NULL, processo, &id1);
+    pthread_create(&t2, NULL, processo, &id2);
+
+    // Mantém o programa rodando enquanto as threads executam
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    // Destroi o semáforo (nunca será alcançado nesse exemplo)
+    sem_destroy(&semaforo);
+
+    return 0;
+}
